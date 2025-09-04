84bf35adaa7be972d965b091134055139c8c14bf
ErikB15 <erik.k.braun@gmail.com>
2025-09-04 17:15:49 +0200

    Fix 6

--- DA339A/Laborationer/Lab2/Lab2Exercise2Fel.java
+++ DA339A/Laborationer/Lab2/Lab2Exercise2Fel.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Lab2Exercise2Fel {
     public static void main(String args[],
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner )= new Scanner(System.in);
         System.out.println("Hej, ange ditt namn!");
         String name = scanner.nextLine(
         System.out.println("Ditt namn är ",  name)
