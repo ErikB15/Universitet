671f1d65dc315cf0a78eb7d7c169947aaaded04f
ErikB15 <erik.k.braun@gmail.com>
2025-09-09 09:09:37 +0200

    Calvulator

---
+++ DA339A/Laborationer/Lab3/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Välkommen till förmodilgen värlsdens bästa kalkylator!");
+        int number1 = 9 / 5;
+        double number2 = 9 / 5;
+        double number3= 9.0 / 5;
+        Double number4 = 9.0 / 5.0;
+        system.out.println (" Tal 1, 9/5 ger: " + number1);
+        system.out.println (" Tal 2, 9/5 ger: " + number2);
+        system.out.println (" Tal 3, 9/5 ger: " + number3);
+        system.out.println (" Tal 4, 9/5 ger: " + number4);
+    }
+}
